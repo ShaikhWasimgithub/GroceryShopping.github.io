73027dd66b574236e9675e35cd8e70552c50d770
Shaikh Wasim Shaikh Aleem <98739500+ShaikhWasimgithub@users.noreply.github.com>
2023-05-09 15:15:10 +0530

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,49 @@
+const list = [
+  {
+    id: 1,
+    title: "Cheese",
+
+    price: 80,
+    img: "https://wallpaperaccess.com/download/cheese-1970465",
+    amount: 1,
+  },
+  {
+    id: 2,
+    title: "Milk",
+
+    price: 40,
+    img: "https://i.pinimg.com/564x/26/a4/74/26a474ff2644e1c3b0190abd7ae9ee41.jpg",
+    amount: 1,
+  },
+  {
+    id: 3,
+    title: "Apple",
+
+    price: 100,
+    img: "https://wallpaperaccess.com/full/1259314.jpg",
+    amount: 1,
+  },
+  {
+    id: 4,
+    title: "Banana",
+    price: 50,
+    img: "https://www.pixelstalk.net/wp-content/uploads/2016/10/Banana-Background-Full-HD.jpg",
+    amount: 1,
+  },
+  {
+    id: 5,
+    title: "Coconut",
+    price: 30,
+    img: "https://images.alphacoders.com/233/233203.jpg",
+    amount: 1,
+  },
+  {
+    id: 6,
+    title: "Canned Beans",
+    price: 75,
+    img: "https://e1.pxfuel.com/desktop-wallpaper/671/745/desktop-wallpaper-bush-s-original-baked-beans-seasoned-with-bacon-brown-sugar-canned-beans-28-oz.jpg",
+    amount: 1,
+  },
+];
+
+export default list;
